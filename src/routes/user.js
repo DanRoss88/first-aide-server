1957f1b96d1ca1b64f2b7f1b454eee1ce2a97bbf
lianaa98 <liana.kan98@gmail.com>
2023-06-09 16:47:07 -0700

    post addusers

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,6 +11,41 @@ userRouter.get("/", async (req, res) => {
   }
 });
 
+userRouter.post("/", async (req, res) => {
+  try {
+    const { username, email, city } = req.body;
+
+    // Check if user already exists
+    const user = await database.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (user.rows.length > 0) {
+      return res.status(401).json({ error: "User already exists." });
+    }
+
+    // Retrieve city information
+    const cityResult = await database.query(
+      "SELECT * FROM city WHERE name = $1",
+      [city]
+    );
+    if (cityResult.rows.length === 0) {
+      return res.status(401).json({ error: "City does not exist." });
+    }
+
+    const cityId = cityResult.rows[0].id;
+
+    // Insert new user
+    const newUser = await database.query(
+      "INSERT INTO users (username, email, city_id) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, cityId]
+    );
+
+    res.json(newUser.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create user.", problem: error });
+  }
+});
+
 module.exports = userRouter;
 
 // const { getAllUsers } = require("../controllers/user_controllers");
