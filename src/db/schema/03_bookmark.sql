da15cca407bce3540ab19950493a4f3c8f9c2605
DanRoss88 <danielmartinross@gmail.com>
2023-06-09 08:41:13 -0700

    bookmark schema fix

--- src/db/schema/03_bookmark.sql
+++ src/db/schema/03_bookmark.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS bookmark CASCADE;
 CREATE TABLE bookmark (
     id SERIAL PRIMARY KEY,
-    aid_id INTEGER REFERENCES aid(id)
+    aid_id INTEGER REFERENCES aid(id),
+    users_id INTEGER REFERENCES users(id)
 );
