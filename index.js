3e40c17012c160a5ca2ed9841685cee0452924c5
lianaa98 <liana.kan98@gmail.com>
2023-06-09 16:45:24 -0700

    middlewares, jwt, morgan

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./src/config/config.db');
+const db = require("./src/config/config.db");
+const jwt = require("jsonwebtoken");
+const morgan = require("morgan");
 
 ///***Require Router Module***///
 // const aidRouter = require('./src/routes/aid');
 // const bookmarkRouter = require('./src/routes/bookmark');
 // const hospitalRouter = require('./src/routes/hospital');
 // const medicalRecordsRouter = require('./src/routes/medical_records');
-// const userRouter = require('./src/routes/user');
-  const loginRouter = require('./src/routes/login');
-  const registerRouter = require('./src/routes/register');
+const userRouter = require("./src/routes/user");
+const loginRouter = require("./src/routes/login");
+const registerRouter = require("./src/routes/register");
 ///***Use Router Module***///
 // app.use('/api',aidRouter);
 // app.use('/api',bookmarkRouter);
@@ -17,18 +19,18 @@ const db = require('./src/config/config.db');
 // app.use('/api',medicalRecordsRouter);
 // app.use('/api',userRouter);
 
-app.use('/login', loginRouter);
-app.use('/register', registerRouter); 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/user", userRouter);
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+
 ///***Start Server***///
 const port = 8000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}
-)
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
