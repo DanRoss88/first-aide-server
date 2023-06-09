998a55f8b63ff86ad5058b1fb73fd5c144a1f55e
lianaa98 <liana.kan98@gmail.com>
2023-06-09 16:46:48 -0700

    get users

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,21 @@
-// const express = require('express');
-// const router = express.Router();
-// const {getAllUsers, getUserById, createUser, updateUser, deleteUser} = require('../controllers/user_controller');
+const express = require("express");
+const userRouter = express.Router();
+const database = require("../config/config.db");
 
-// router.get('/users', getAllUsers);
+userRouter.get("/", async (req, res) => {
+  try {
+    const users = await database.query("SELECT * FROM users");
+    res.json(users.rows);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve users." });
+  }
+});
+
+module.exports = userRouter;
+
+// const { getAllUsers } = require("../controllers/user_controllers");
+// router.get("/", getAllUsers);
 // router.get('/users/:id', getUserById);
 // router.post('/users', createUser);
 // router.put('/users/:id', updateUser);
 // router.delete('/users/:id', deleteUser);
-
-
-// module.exports = router;
