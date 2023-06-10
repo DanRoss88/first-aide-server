59b75e8bcca2a30ee5556048da1ea8b4be202345
DanRoss88 <danielmartinross@gmail.com>
2023-06-09 21:21:18 -0700

    .env

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-
-const db = require("./src/config/config.db");
+const database = require("./src/config/config.db");
 const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 
-
+require("dotenv").config();
 
 ///***Require Router Module***///
 
@@ -23,12 +22,16 @@ const mRRouter = require("./src/routes/medical_records");
 // app.use('/api',medicalRecordsRouter);
 // app.use('/api',userRouter);
 
+const userRouter = require("./src/routes/user");
+
 ///***Use Middleware***///
 app.use(express.json());
 app.use(morgan("dev"));
 
 
 ///***Use Router Module***///
+// app.use(authenticateToken);
+
 app.use("/users", userRouter);
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
@@ -41,7 +44,49 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/test", (req, res) => {
+  res.send("Hello " + req.payload.name);
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.email;
+  console.log("helloooo", req.body);
+
+  // check if email exists in database
+  const user = await database.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (user.rows.length === 0) {
+    return res.status(401).json({ error: "User does not exist." });
+  }
+
+  // email exists :)
+  const payload = {
+    user_id: user.rows[0].id,
+    email: user.rows[0].email,
+    name: user.rows[0].username,
+  };
+
+  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+  if (req.path === "/login" || req.path === "/register") {
+    return next();
+  }
+
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+    if (err) return res.sendStatus(403);
+    req.payload = payload;
+    next();
+  });
+}
