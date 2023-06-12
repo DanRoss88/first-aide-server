4a787a738b2c7768388e987183ec0d4dcb692836
lianaa98 <liana.kan98@gmail.com>
2023-06-12 11:07:13 -0700

    fix get emerg contact route

--- src/routes/emergency_contact.js
+++ src/routes/emergency_contact.js
@@ -1,57 +1,65 @@
-const express = require('express');
+const express = require("express");
 const emergContRouter = express.Router();
-const { getAllEmergencyContacts, createEmergencyContact, deleteEmergencyContact } = require('../controllers/emergency_contact_controllers');
-
+const {
+  getAllEmergencyContacts,
+  createEmergencyContact,
+  deleteEmergencyContact,
+} = require("../controllers/emergency_contact_controllers");
+const jwt = require("jsonwebtoken");
 
 ///// Emergency Contact Routes /////
 
 // Get all emergency contacts by user ID
 
-emergContRouter.get('/:userId', (req, res) => {
-  const userId = req.params.userId;
+emergContRouter.get("/", (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  console.log(token);
+  let userId;
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+    if (err) return res.sendStatus(403).json({ error: "Invalid token." });
+    userId = payload.user_id;
+  });
 
   getAllEmergencyContacts(userId)
     .then((data) => {
       res.json(data);
     })
     .catch((error) => {
-      res.status(500).json({ error: 'An error occurred' });
+      res.status(500).json({ error: "An error occurred" });
     });
-  }
-);
+});
 
 // Create a new emergency contact
 
-emergContRouter.post('/', (req, res) => {
-
+emergContRouter.post("/", (req, res) => {
   const { userId, contactName, phone, relationship } = req.body;
 
   createEmergencyContact(userId, contactName, phone, relationship)
     .then((data) => {
       res.json(data);
-      console.log('Emergency Contact created');
+      console.log("Emergency Contact created");
     })
     .catch((error) => {
-      res.status(500).json({ error: 'An error occurred' });
+      res.status(500).json({ error: "An error occurred" });
     });
-}
-);
+});
 
 // Delete user emergency contact by ID
 
-emergContRouter.delete('/:emergencyContactId', (req, res) => {
+emergContRouter.delete("/:emergencyContactId", (req, res) => {
   const emergencyContactId = req.params.emergencyContactId;
   const userId = req.body.userId;
 
   deleteEmergencyContact(emergencyContactId, userId)
     .then((data) => {
       res.json(data);
-      console.log('Emergency Contact deleted');
+      console.log("Emergency Contact deleted");
     })
     .catch((error) => {
-      res.status(500).json({ error: 'An error occurred' });
+      res.status(500).json({ error: "An error occurred" });
     });
-}
-);
+});
 
 module.exports = emergContRouter;
